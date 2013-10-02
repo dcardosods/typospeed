496d410338a5b369a9a4684a098ed950546c64d0
Daniel Cardoso <danielcardosods@gmail.com>
2013-10-02 16:08:32 -0300

    Get random word using YQL, and add in DOM

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,11 +7,29 @@
         canvas = document.getElementById('canvas');
 
     var setWords = function (data) {
-        for (var i = data.list.length - 1; i >= 0; i--) {
-            words[data.list[i]] = '';
-        }
+        if (data.query.results && data.query.results.results) {
+            var usableData = data.query.results.results.h3;
 
-        writeWords(words);
+            if (Array.isArray(usableData)) {
+                if (usableData[0].content) {
+                    words[usableData[0].content] = '';
+                }
+                else {
+                    words[usableData[0]] = '';
+                }
+            }
+            else if (usableData.content) {
+                words[usableData.content] = '';
+            }
+            else {
+                words[usableData] = '';
+            }
+
+            writeWords(words);
+        }
+        else {
+            console.log('Words not found!');
+        }
     };
 
     var writeWords = function (words) {
@@ -29,6 +47,10 @@
         }
     };
 
-    loadJSONP('http://dicionario-aberto.net/search-json?prefix=ab', setWords);
-
+    loadJSONP('http://query.yahooapis.com/v1/public/yql?' +
+        'q=use%0A%22http%3A%2F%2Fyqlblog.net%2Fsamples%2Fdata.html.cssselect.xml%22' +
+        '%20as%20data.html.cssselect%3B%0Aselect%20*%20from%20data.html.cssselect' +
+        '%20where%20url%3D%22http%3A%2F%2Fwww.dicionario-aberto.net%2Frandom%22' +
+        '%20and%0Acss%3D%22%23main%20.text%20.term%20h3%22&format=json',
+        setWords);
 })(loadJSONP);
