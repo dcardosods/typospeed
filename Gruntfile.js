679153773f4d6c41a3ca5cb6eacbe3aeabad13e1
Daniel Cardoso <danielcardosods@gmail.com>
2013-10-03 19:06:34 -0300

    Remove unnecessary minimatch negation on grunt jshint config

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
             options: {
                 jshintrc: '.jshintrc'
             },
-            all: ['Gruntfile.js', 'app/scripts/*.js', '!app/scripts/vendor/*']
+            all: ['Gruntfile.js', 'app/scripts/*.js']
         },
         githooks: {
             all: {
