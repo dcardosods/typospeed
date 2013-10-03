9596863bfdd39356727518ae4d2781cf65bc0a8f
Daniel Cardoso <danielcardosods@gmail.com>
2013-10-03 16:09:30 -0300

    Prevent timeout for insert the first word

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,12 @@
 (function (loadJSONP) {
 
     var words = {},
-        canvas = document.getElementById('canvas');
+        canvas = document.getElementById('canvas'),
+        wordsQuery = 'http://query.yahooapis.com/v1/public/yql?' +
+            'q=use%0A%22http%3A%2F%2Fyqlblog.net%2Fsamples%2Fdata.html.cssselect.xml%22' +
+            '%20as%20data.html.cssselect%3B%0Aselect%20*%20from%20data.html.cssselect' +
+            '%20where%20url%3D%22http%3A%2F%2Fwww.dicionario-aberto.net%2Frandom%22' +
+            '%20and%0Acss%3D%22%23main%20.text%20.term%20h3%22&format=json';
 
     var setWords = function (data) {
         if (data.query.results && data.query.results.results) {
@@ -50,13 +55,9 @@
         canvas.appendChild(wordNode);
     };
 
+    loadJSONP(wordsQuery,setWords);
     window.setInterval(function () {
-        loadJSONP('http://query.yahooapis.com/v1/public/yql?' +
-        'q=use%0A%22http%3A%2F%2Fyqlblog.net%2Fsamples%2Fdata.html.cssselect.xml%22' +
-        '%20as%20data.html.cssselect%3B%0Aselect%20*%20from%20data.html.cssselect' +
-        '%20where%20url%3D%22http%3A%2F%2Fwww.dicionario-aberto.net%2Frandom%22' +
-        '%20and%0Acss%3D%22%23main%20.text%20.term%20h3%22&format=json',
-        setWords);
+        loadJSONP(wordsQuery,setWords);
     }, 10000);
 
 })(loadJSONP);
